30702d16a3e05d095bc1d05869c224d93cf8b815
Alexander Petkov <sashko506@gmail.com>
2023-09-24 00:32:17 +0300

    DailyForecastScreen.tsx: Fix borders overlap

--- src/components/forecast/DailyForecastScreen.tsx
+++ src/components/forecast/DailyForecastScreen.tsx
@@ -19,14 +19,14 @@ export default function DailyForecastScreen() {
   };
 
   return (
-    <div className="flex relative flex-row bg-white">
-      <div className="hidden sm:flex flex-col w-full h-full min-w-[150px] border-r-2 border-dashed p-2 bg-white">
-        <div className="font-bold text-lg text-right">Час</div>
-        <div className="text-[#CACBCC] text-right">Дата</div>
-        <div className="flex flex-col mt-16 justify-center items-center"></div>
-        <div className="h-full">
-          <ul className="text-right flex gap-[0.7rem] flex-col pb-14 h-full justify-end">
-            <li className="mb-9">Скорост на вятъра</li>
+    <div className='flex relative flex-row bg-white'>
+      <div className='hidden sm:flex flex-col w-full h-full min-w-[150px] border-r-2 border-dashed p-2 bg-white'>
+        <div className='font-bold text-lg text-right'>Час</div>
+        <div className='text-[#CACBCC] text-right'>Дата</div>
+        <div className='flex flex-col mt-16 justify-center items-center'></div>
+        <div className='h-full'>
+          <ul className='text-right flex gap-[0.7rem] flex-col pb-14 h-full justify-end'>
+            <li className='mb-9'>Скорост на вятъра</li>
             <li>Усеща се</li>
             <li>Облачност</li>
             <li>Атм. налягане</li>
@@ -34,8 +34,8 @@ export default function DailyForecastScreen() {
           </ul>
         </div>
       </div>
-      <div className="flex w-full sm:w-fit  flex-col  sm:flex-row overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-rounded-[100px] scrollbar-track-gray-100">
-        <button className="py-4 sm:hidden" onClick={toggleMetricForm}>
+      <div className='flex w-full sm:w-fit  flex-col  sm:flex-row overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-rounded-[100px] scrollbar-track-gray-100'>
+        <button className='py-4 sm:hidden' onClick={toggleMetricForm}>
           {" "}
           + Добави нови измервания
         </button>
@@ -44,7 +44,7 @@ export default function DailyForecastScreen() {
             !showMetricForm ? "grid-rows-[0fr]" : "grid-rows-[1fr]"
           }`}
         >
-          <div className="overflow-hidden relative">
+          <div className='overflow-hidden relative'>
             <AddNewMetricScreen hideForm={toggleMetricForm} />
           </div>
         </div>
@@ -94,52 +94,52 @@ function DailyForecastCard({
 }: any) {
   const hourAndDate = getHoursAndDate(elem.dt, offset);
   return (
-    <>
-      <div className="flex flex-row gap-1 sm:pb-12 border-t-2 first:border-t-0 sm:border-t-0 sm:flex-col items-center justify-between first:border-l-0 border-l-2 border-dashed p-2 bg-white">
-        <div className="flex flex-row w-full sm:flex-col items-center justify-between">
-          <div className="flex flex-col">
-            <div className="font-bold text-sm sm:text-lg sm:text-center">
+    <div className='first:border-r-2 border-r-2 border-t-2 sm:border-t-0  border-dashed'>
+      <div className='flex flex-row gap-1 sm:pb-12 sm:flex-col items-center justify-between  p-2 bg-white'>
+        <div className='flex flex-row w-full sm:flex-col items-center justify-between'>
+          <div className='flex flex-col'>
+            <div className='font-bold text-sm sm:text-lg sm:text-center'>
               {hourAndDate.hour}
             </div>
-            <div className="text-[#CACBCC] text-sm sm:text-lg sm:text-center">
+            <div className='text-[#CACBCC] text-sm sm:text-lg sm:text-center'>
               {hourAndDate.date}
             </div>
           </div>
-          <div className="flex flex-row  items-center gap-1 sm:flex-col sm:my-[2rem]">
-            <div className="relative  w-[50px] min-[450px]:w-32 aspect-square">
+          <div className='flex flex-row  items-center gap-1 sm:flex-col sm:my-[2rem]'>
+            <div className='relative  w-[50px] min-[450px]:w-32 aspect-square'>
               <Image
                 src={openWeatherImageUrl(elem.weather[0].icon)}
                 fill
-                alt="weather-icon"
+                alt='weather-icon'
               />
             </div>
-            <div className="flex flex-row justify-center">
-              <span className="text-orange-500 text-xl sm:text-4xl break-normal">
+            <div className='flex flex-row justify-center'>
+              <span className='text-orange-500 text-xl sm:text-4xl break-normal'>
                 {Math.floor(elem.temp)}{" "}
               </span>
-              <span className="text-orange-500 text-xl sm:text-2xl whitespace-normal">
+              <span className='text-orange-500 text-xl sm:text-2xl whitespace-normal'>
                 °C
               </span>
             </div>
             <button
-              className="sm:hidden"
+              className='sm:hidden'
               onPointerOver={onPointerOver}
               onPointerOut={onPointerOut}
             >
-              <InformationIcon styles="w-10 h-9 fill-[#C8C9CA] stroke-white" />
+              <InformationIcon styles='w-10 h-9 fill-[#C8C9CA] stroke-white' />
             </button>
           </div>
         </div>
-        <div className="hidden sm:flex flex-col mt-[1rem] justify-center items-center">
-          <div className=" relative w-10 aspect-square">
-            <Image src={"/icons/windIcon.png"} fill alt="wind-icon" />
+        <div className='hidden sm:flex flex-col mt-[1rem] justify-center items-center'>
+          <div className=' relative w-10 aspect-square'>
+            <Image src={"/icons/windIcon.png"} fill alt='wind-icon' />
           </div>
-          <span className="text-sm text-[#C4E6F5]">
+          <span className='text-sm text-[#C4E6F5]'>
             {Math.ceil(elem.wind_speed)} м/с
           </span>
         </div>
-        <div className="mt-[2rem] mb-[0.6rem]">
-          <ul className="hidden sm:flex flex-col gap-[0.6rem] text-center">
+        <div className='mt-[2rem] mb-[0.6rem]'>
+          <ul className='hidden sm:flex flex-col gap-[0.6rem] text-center'>
             <li>{elem.feels_like} C</li>
             <li>{elem.clouds}%</li>
             <li>{elem.pressure} hPa</li>
@@ -154,6 +154,6 @@ function DailyForecastCard({
       >
         <WeatherTooltip weatherData={elem} />
       </div>
-    </>
+    </div>
   );
 }
