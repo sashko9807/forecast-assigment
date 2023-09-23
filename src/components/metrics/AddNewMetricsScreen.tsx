1d398649f34d01c6a80d9ac553f1482f4531064e
Alexander Petkov <sashko506@gmail.com>
2023-09-23 21:01:53 +0300

    AddNewMetricsScreen.tsx: Fix validation

--- src/components/metrics/AddNewMetricsScreen.tsx
+++ src/components/metrics/AddNewMetricsScreen.tsx
@@ -31,122 +31,122 @@ export default function AddNewMetricScreen({ hideForm }: any) {
   };
 
   return (
-    <div className="py-5">
-      <div className="sm:absolute right-0 bottom-[0.50rem]"></div>
-      <h1 className="hidden  text-center font-bold w-full sm:flex flex-row justify-center py-5">
+    <div className='py-5'>
+      <div className='sm:absolute right-0 bottom-[0.50rem]'></div>
+      <h1 className='hidden  text-center font-bold w-full sm:flex flex-row justify-center py-5'>
         Добавяне на показател
       </h1>
       <button
-        className="hidden sm:block absolute right-2 top-2 text-[22px] text-red-500"
+        className='hidden sm:block absolute right-2 top-2 text-[22px] text-red-500'
         onClick={hideForm}
       >
         X
       </button>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col justify-center gap-3 px-5 w-full">
-          <label className="flex flex-col">
+        <div className='flex flex-col justify-center gap-3 px-5 w-full'>
+          <label className='flex flex-col'>
             Username:
             <input
-              type="text"
+              type='text'
               className={`rounded-lg ${
                 errors.username && " border-2 border-red-500"
               } bg-slate-200 px-4`}
               {...register("username")}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.username && errors.username.message}
             </p>
           </label>
           <label className={`flex flex-col`}>
             Email:
             <input
-              type="email"
+              type='email'
               className={`rounded-lg ${
                 errors.email && " border-2 border-red-500"
               } bg-slate-200 px-4`}
               {...register("email")}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.email && errors.email.message}
             </p>
           </label>
           <label className={`flex flex-col`}>
             Temperature:
             <input
-              type="text"
-              inputMode="numeric"
+              type='text'
+              inputMode='numeric'
               className={`rounded-lg ${
                 errors.temperature && "border-2 border-red-500"
               } px-4 bg-slate-200`}
-              {...register("temperature")}
+              {...register("temperature", { valueAsNumber: true })}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.temperature && errors.temperature.message}
             </p>
           </label>
-          <label className="flex flex-col">
+          <label className='flex flex-col'>
             Wind speed:
             <input
-              type="text"
-              inputMode="numeric"
+              type='text'
+              inputMode='numeric'
               className={`rounded-lg ${
                 errors.wind_speed && " border-2 border-red-500"
               } px-4 bg-slate-200`}
-              {...register("wind_speed")}
+              {...register("wind_speed", { valueAsNumber: true })}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.wind_speed && errors.wind_speed.message}
             </p>
           </label>
-          <label className="flex flex-col">
+          <label className='flex flex-col'>
             Humidity:
             <input
-              type="text"
-              inputMode="numeric"
+              type='text'
+              inputMode='numeric'
               className={`rounded-lg ${
                 errors.humidity && "border-2 border-red-500"
               } px-4 bg-slate-200`}
-              {...register("humidity")}
+              {...register("humidity", { valueAsNumber: true })}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.humidity && errors.humidity.message}
             </p>
           </label>
-          <label className="flex flex-col">
+          <label className='flex flex-col'>
             Pressure:
             <input
-              type="text"
-              inputMode="numeric"
+              type='text'
+              inputMode='numeric'
               className={`rounded-lg ${
                 errors.pressure && "border-2 border-red-500"
               } px-4 bg-slate-200`}
-              {...register("pressure")}
+              {...register("pressure", { valueAsNumber: true })}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.pressure && errors.pressure.message}
             </p>
           </label>
-          <label className="flex flex-col">
+          <label className='flex flex-col'>
             Rain:
             <input
-              type="text"
-              inputMode="numeric"
+              type='text'
+              inputMode='numeric'
               className={`rounded-lg ${
                 errors.rain && "border-2 border-red-500"
               } px-4 bg-slate-200`}
-              {...register("rain")}
+              {...register("rain", { valueAsNumber: true })}
             />
-            <p className="text-red-500 text-[12px]">
+            <p className='text-red-500 text-[12px]'>
               {errors.rain && errors.rain.message}
             </p>
           </label>
           <input
-            type="submit"
-            className="bg-orange-500 p-2  rounded-lg justify-center text-white"
-            value="Изпрати"
+            type='submit'
+            className='bg-orange-500 p-2  rounded-lg justify-center text-white'
+            value='Изпрати'
           />
         </div>
-        <div className="w-full justify-center flex"></div>
+        <div className='w-full justify-center flex'></div>
       </form>
     </div>
   );
